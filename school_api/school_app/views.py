96c2087a1a0b0e2554b1004bab9d54f7ef9ca3f6
phill9242 <phil@Dell-G3>
2023-02-28 19:22:06 -0300

    checkagem de formulario durante edição completa

--- school_api/school_app/views.py
+++ school_api/school_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DeleteView, UpdateView
-from .forms import formAluno, formProfessor, formCurso, EditarAlunoForm, EditarCursoForm, EditarProfessorForm
+from .forms import formAluno, formProfessor, formCurso, EditarAlunoForm, EditarCursoForm, EditarProfessorForm, validar_cpf
 from .models import Aluno, Curso, Professor
 from django.urls import reverse_lazy
 from .cadastro import processar_formulario
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
 class ListarAlunosView(ListView):
 	model = Aluno
@@ -17,6 +20,29 @@ class EditarAlunoView(UpdateView):
 
 	success_url = reverse_lazy('listar_alunos')
 
+	def form_valid(self, form):
+		aluno = form.save(commit=False)
+		cpf = form.cleaned_data['cpf']
+		try:
+			validar_cpf(cpf)
+		except IntegrityError:
+			form.add_error('cpf', 'Já existe um registro com este CPF.')
+			return self.form_invalid(form)
+		except ValidationError as error:
+			messages.info(self.request, error.message)
+			return self.form_invalid(form)
+		
+		aluno.save()
+		messages.success(self.request, 'Aluno atualizado com sucesso!')
+		return super().form_valid(form)
+	
+	def form_invalid(self, form):
+		response = super().form_invalid(form)
+		if 'cpf' in form.errors:
+			form.errors['cpf'] = None
+			messages.info(self.request, 'Já existe um registro com este CPF.')
+		return response
+	
 class DeletarAlunoView(DeleteView):
 	model = Aluno
 	success_url = reverse_lazy('listar_alunos')
@@ -31,7 +57,7 @@ class EditarCursoView(UpdateView):
 	form_class = EditarCursoForm
 	template_name = 'editar_curso.html'
 	success_url = reverse_lazy('listar_cursos')
-
+	
 class DeletarCursoView(DeleteView):
 	model = Curso
 	success_url = reverse_lazy('listar_cursos')
@@ -46,7 +72,30 @@ class EditarProfessorView(UpdateView):
 	form_class = EditarProfessorForm
 	template_name = 'editar_professor.html'
 	success_url = reverse_lazy('listar_professores')
-
+	
+	def form_valid(self, form):
+		professor = form.save(commit=False)
+		cpf = form.cleaned_data['cpf']
+		try:
+			validar_cpf(cpf)
+		except IntegrityError:
+			form.add_error('cpf', 'Já existe um registro com este CPF.')
+			return self.form_invalid(form)
+		except ValidationError as error:
+			messages.info(self.request, error.message)
+			return self.form_invalid(form)
+		
+		professor.save()
+		messages.success(self.request, 'Professor atualizado com sucesso!')
+		return super().form_valid(form)
+	
+	def form_invalid(self, form):
+		response = super().form_invalid(form)
+		if 'cpf' in form.errors:
+			form.errors['cpf'] = None
+			messages.info(self.request, 'Já existe um registro com este CPF.')
+		return response
+	
 class DeletarProfessorView(DeleteView):
 	model = Professor
 	success_url = reverse_lazy('listar_professores')
